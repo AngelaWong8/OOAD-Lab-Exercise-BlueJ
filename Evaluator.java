7e599d10e08d2e5f2a34756f71e90b283f718ced
4zlee <165985358+4zlee@users.noreply.github.com>
2026-01-17 01:10:57 +0800

    Update Evaluator.java

--- Evaluator.java
+++ Evaluator.java
@@ -5,11 +5,14 @@ public class Evaluator extends User {
     }
 
     public void reviewPresentation() {
+        System.out.println(getName() + " is reviewing the submission...");
     }
 
     public void evaluateStudents() {
+        System.out.println("Evaluator " + getName() + " has triggered student evaluation.");
     }
 
-    public void comment() {
+    public void comment(Evaluation eval, String commentText) {
+        System.out.println("Evaluator is adding a comment to the evaluation record.");
     }
 }
